cdf16eec5ed67b4a79cda556d100f13196f8fd44
Kim, Seongjun <sjkim@kdb.snu.ac.kr>
2018-07-09 15:44:42 +0900

    RESTful API draft

---
+++ sensor_data/example.py
@@ -0,0 +1,70 @@
+from requests import post
+import json
+
+credential = "<code>"
+
+# API url format
+#  - http://147.47.220.28:5000/<command>/<sensor type>/<id>
+#  - command: new, get, get_all, delete, delete_all
+#  - sensor type: PM1, PM10, PM25, HUM
+
+# Response field (common)
+#  - status: if success, "OK". otherwise "ERROR"
+#  - message: only for 'ERROR'. Error description
+
+#-----------------------------------------------
+# 1. Insert new value
+#-----------------------------------------------
+for i in range(5):
+    new_data = """{"sensor_name":"PM25", "name":"pm2007", "timestamp": "18-05-11 14:34:56.382242", "latitude": 37.45, "longitude": 126.8, "value":12.7}"""
+    ret = post('http://localhost:5000/new/PM25', data={"data": new_data, "credential": credential}).json()
+    new_id = ret['id']
+    print("New value is inserted, ID: {0}".format(new_id))
+
+# Response field
+#  - status
+#  - id: id of inserted row(value)
+
+#-----------------------------------------------
+# 2. Retrieve one value
+#-----------------------------------------------
+ret = post('http://localhost:5000/get/PM25/{0}'.format(new_id), data={"credential": credential}).json()
+value = json.loads(ret['data'])
+print("ID {0} value is retrieved: {1}".format(new_id, value))
+
+# Response field
+#  - status
+#  - count: number of retrieved row
+#  - data: json string of retrieved row
+
+#-----------------------------------------------
+# 3. Retrieve all values
+#-----------------------------------------------
+ret = post('http://localhost:5000/get_all/PM25', data={"credential": credential}).json()
+values = json.loads(ret['data'])
+print("{0} values are retrieved".format(len(values)))
+
+# Response field
+#  - status
+#  - count: number of retrieved rows
+#  - data: json string of retrieved rows
+
+#-----------------------------------------------
+# 4. Delete one value
+#-----------------------------------------------
+ret = post('http://localhost:5000/delete/PM25/{0}'.format(new_id), data={"credential": credential}).json()
+print("{0} value is deleted".format(ret['count']))
+
+# Response field
+#  - status
+#  - count: number of deleted row
+
+#-----------------------------------------------
+# 5. Delete all
+#-----------------------------------------------
+ret = post('http://localhost:5000/delete_all/PM25', data={"credential": credential}).json()
+print("{0} values are deleted".format(ret['count']))
+
+# Response field
+#  - status
+#  - count: number of deleted rows
---
+++ sensor_data/main.py
@@ -0,0 +1,151 @@
+from flask import Flask, g
+from flask_restful import Resource, Api, reqparse
+import mysql.connector
+
+import json
+
+parser = reqparse.RequestParser()
+parser.add_argument('data')
+parser.add_argument('credential')
+
+app = Flask(__name__)
+api = Api(app)
+
+
+#############
+## Configuration
+#############
+from dbms_config import *
+from credential import *
+
+#############
+## Utilities
+#############
+def connect_db():
+    cnx = mysql.connector.connect(user=DBMS_USER, password=DBMS_PASSWD,
+            host=DBMS_HOST,
+            database=DBMS_DATABASE,
+            port=DBMS_PORT)
+    cursor = cnx.cursor()
+
+    return (cnx, cursor)
+
+def get_db():
+    if not hasattr(g, 'db_cursor'):
+        conn, cursor = connect_db()
+        g.db_conn = conn
+        g.db_cursor = cursor
+    return g.db_cursor
+
+def check_credential(code):
+    if code is None:
+        return (False, "Please provide your creential code")
+    elif code == allowed_codes[1]:
+        return (True, "")
+    else:
+        return (False, "Invalid credential code")
+
+#############
+## API
+#############
+class NewValue(Resource):
+    def post(self, sensor_name):
+        args = parser.parse_args()
+        (allowed, msg) = check_credential(args['credential'])
+        if not allowed:
+            return {"status": "ERROR", "message": msg}, 400
+
+        data = json.loads(args['data'])
+
+        cursor = get_db()
+        cursor.execute("""
+            INSERT INTO {0} (name, timestamp, latitude, longitude, value) 
+            VALUES (%s, %s, %s, %s, %s)
+            """.format(sensor_name), 
+            (data['name'], data['timestamp'], data['latitude'], data['longitude'], data['value']))
+
+        new_id = cursor.lastrowid
+
+        return {"status": "OK", "id": new_id}
+
+class GetValue(Resource):
+    def post(self, sensor_name, id):
+        args = parser.parse_args()
+        (allowed, msg) = check_credential(args['credential'])
+        if not allowed:
+            return {"status": "ERROR", "message": msg}, 400
+
+        cursor = get_db()
+        cursor.execute("""
+        SELECT * FROM {0} where id = %s
+        """.format(sensor_name), (id,))
+
+        row = cursor.fetchone()
+        if row is None:
+            return {"status": "ERROR", "message": "Invalid id"}
+
+        (id, name, timestamp, latitude, longitude, value) = row
+        timestamp = str(timestamp)
+        row = {"id": id, "name": name, "count": 1, "timestamp": timestamp, "latitude": latitude, "longitude": longitude, "value": value}
+
+        return {"status": "OK", "count": cursor.rowcount, "data": json.dumps(row)}
+
+class GetAllValues(Resource):
+    def post(self, sensor_name):
+        args = parser.parse_args()
+        (allowed, msg) = check_credential(args['credential'])
+        if not allowed:
+            return {"status": "ERROR", "message": msg}, 400
+
+        cursor = get_db()
+        cursor.execute("""
+        SELECT * FROM {0} order by id
+        """.format(sensor_name))
+
+        rows = cursor.fetchall()
+        rows_json = []
+        for (id, name, timestamp, latitude, longitude, value) in rows:
+            timestamp = str(timestamp)
+            rows_json.append({"id": id, "name": name, "timestamp": timestamp, "latitude": latitude, "longitude": longitude, "value": value})
+
+        return {"status": "OK", "count": cursor.rowcount, "data": json.dumps(rows_json)}
+
+class DeleteValue(Resource):
+    def post(self, sensor_name, id):
+        args = parser.parse_args()
+        (allowed, msg) = check_credential(args['credential'])
+        if not allowed:
+            return {"status": "ERROR", "message": msg}, 400
+
+        cursor = get_db()
+        cursor.execute("""
+        DELETE FROM {0} where id = %s
+        """.format(sensor_name), (id,))
+
+        if cursor.rowcount == 0:
+            return {"status": "ERROR", "message": "Invalid id"}
+
+        return {"status": "OK", "count": cursor.rowcount}
+
+class DeleteAllValues(Resource):
+    def post(self, sensor_name):
+        args = parser.parse_args()
+        (allowed, msg) = check_credential(args['credential'])
+        if not allowed:
+            return {"status": "ERROR", "message": msg}, 400
+
+        cursor = get_db()
+        cursor.execute("""
+        DELETE FROM {0}
+        """.format(sensor_name))
+
+        return {"status": "OK", "count": cursor.rowcount}
+
+api.add_resource(NewValue, '/new/<string:sensor_name>')
+api.add_resource(GetValue, '/get/<string:sensor_name>/<int:id>')
+api.add_resource(GetAllValues, '/get_all/<string:sensor_name>')
+api.add_resource(DeleteValue, '/delete/<string:sensor_name>/<int:id>')
+api.add_resource(DeleteAllValues, '/delete_all/<string:sensor_name>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
